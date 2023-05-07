0bcae65bba39122654bfaeba62bf9cb2af522d93
shubham shekhar <shubhamshekhar567@gmail.com>
2023-05-08 04:22:05 +0530

    scaffolding the project done with react, tailwind and electron

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-4xl mb-4">Electron, React, and Tailwind CSS</h1>
+        <p className="text-xl">This is an example of using Electron, React, and Tailwind CSS together.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  darkMode: "class", // or 'media' or 'class
+  theme: {
+    extend: {},
+  },
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  plugins: [],
+}
+
